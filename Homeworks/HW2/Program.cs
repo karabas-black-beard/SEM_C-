90b8f6da256f183286909aecd4de9fb398025c8f
karabas <gustov.andrei@mail.ru>
2023-07-02 21:44:08 +0300

    домашка  2

---
+++ Homeworks/HW2/Program.cs
@@ -0,0 +1,60 @@
+﻿//задача 1 прога из 3 значного выводит вторую цифру
+
+// int GetNum(int num)
+// {
+//     if (num > 99 && num < 1000)
+//     {
+//         int res = num / 10 % 10;
+//         return res;
+
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("ввели не трехзначное число");
+//         return 0;
+//     }
+// }
+// System.Console.Write("введите трехзначное число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(GetNum(num));
+
+
+
+//задача 2 прога ввывоит третью цифру с конца, сообщать если её нет
+// int PrintThird(int num)
+// {
+//     while (num > 999)
+//     {
+//         num = num / 10;
+//     }
+//     return num % 10;
+// }
+// System.Console.Write("введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num < 100)
+// {
+//     System.Console.WriteLine("третьего числа нет");
+// }
+// System.Console.WriteLine(PrintThird(num));  
+
+
+//задача 3 прога  которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+bool ValidateDay(int day)
+{
+    if (day > 7)
+    {
+        System.Console.WriteLine("такого дня недели нет");
+        return false;
+    }
+    return true;
+}
+void GetDay(int day)
+{
+    if (day == 6 || day == 7) System.Console.WriteLine("выходной день");
+    else System.Console.WriteLine("рабочий день");
+}
+System.Console.WriteLine("введите номер дня недели:");
+int day = Convert.ToInt32(Console.ReadLine());
+if (ValidateDay(day)) GetDay(day);
+
--- Seminars/Sem2/Program.cs
+++ Seminars/Sem2/Program.cs
@@ -59,7 +59,6 @@
 // int num1 = Convert.ToInt32(Console.ReadLine());
 // System.Console.Write("введите второе число:");
 // int num2 = Convert.ToInt32(Console.ReadLine());
-
 // void VentNum(int num1, int num2)
 // {
 //     if (num1 == num2 * num2)
